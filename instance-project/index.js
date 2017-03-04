598ea8575eaf52c2c29efd3b0931310f4f232535
davidkatz <15Dkatz@shcp.edu>
2017-03-04 12:07:01 -0800

    add conditional and list rendering examples

--- instance-project/index.js
+++ instance-project/index.js
@@ -50,3 +50,67 @@ var app3 = new Vue({
     }
   }
 })
+
+// conditional rendering
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    primary: true,
+    showTemplate: true,
+    number: 1,
+    infoType: 'email'
+  },
+  methods: {
+    toggleInfoType: function() {
+      console.log('toggling type');
+      this.infoType = this.infoType === 'email' ? 'phone number' : 'email';
+    }
+  }
+})
+
+// list rendering
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    tasks: [
+      { title: 'Read Ruby 1 '},
+      { title: 'Read Ruby 2 '}
+    ],
+    vader: {
+      firstName: 'Darth',
+      lastName: 'Vader',
+      color: 'red'
+    }
+  }
+})
+
+
+// list rendering on components
+Vue.component('task-item', {
+  template: `
+    <li>
+      {{task.title}}
+    </li>
+  `,
+  props: ['task']
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    newTask: '',
+    tasks: [
+      { title: 'Read Ruby 1', isDone: false },
+      { title: 'Read Ruby 2', isDone: true }
+    ],
+    numbers: [1, 2, 3, 4, 5, 6]
+  },
+  methods: {
+    addTask: function() {
+      this.tasks.push({title: this.newTask, isDone: false})
+    },
+    oddsOnly: function(array) {
+      return array.filter(i => i % 2 == 0 ? '' : i)
+    }
+  }
+})
