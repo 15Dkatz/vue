716630eacaf60945e06a83f01035c8aeb5bc2098
davidkatz <15Dkatz@shcp.edu>
2017-03-16 21:28:43 -0700

    add starbase

---
+++ udemy/starbase/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h3 class="">Select a Character</h3>
+    <Match />
+  </div>
+</template>
+
+<script>
+import Match from './components/Match.js'
+export default {
+  name: 'app',
+  components: {
+    Match
+  }
+}
+</script>
+
+
+<!-- Emit the select function based on the child match component -->
